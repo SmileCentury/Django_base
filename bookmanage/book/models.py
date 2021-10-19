c4b2eb56ded717e4de37fdba3308c017c0bb8d3d
myHome <805222631@qq.com>
2021-10-19 23:09:11 +0800

    项目准备

--- bookmanage/book/models.py
+++ bookmanage/book/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+    b_title = models.CharField(max_length=20,verbose_name='书名')
+    pub_date = models.DateField(verbose_name='发行日期',null=True)
+    readcount = models.IntegerField(verbose_name='阅读量',default=0)
+    commentcount = models.IntegerField(verbose_name='评论量',default=0)
+    is_delete = models.BooleanField(default=False,verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '图书'
+
+    def __str__(self):
+        return self.b_title
+
+class People_Info(models.Model):
+    GENDER_CHIOCE = ((0,'male'),(1,'female'))
+    name = models.CharField(max_length=20,verbose_name='姓名')
+    gender = models.SmallIntegerField(choices=GENDER_CHIOCE,default=0,verbose_name='性别')
+    description = models.CharField(max_length=200,null=True,verbose_name='描述信息')
+    book = models.ForeignKey(to=BookInfo,on_delete=models.CASCADE,verbose_name='图书')
+    is_delete = models.BooleanField(verbose_name='逻辑删除',default=False)
+
+    class Meta:
+        db_table = 'people_info'
+        verbose_name ='人物'
+    def __str__(self):
+        return self.name
+
+
--- bookmanage/bookmanage/settings.py
+++ bookmanage/bookmanage/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book.apps.BookConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'bookmanage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "USER":'root',
+        'PASSWORD':'mysql',
+        'HOST':'localhost',
+        'PORT':3306,
+        'NAME':'bookmanage',
+
     }
 }
 
@@ -103,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
